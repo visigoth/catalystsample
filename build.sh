c2c3a57fe6fbf1e062151a9427056d30b6bd13be
Shaheen Gandhi <shaheen@fb.com>
2020-12-19 18:31:00 -0800

    Use -Z build-std instead

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Add --release for release builds
-xargo build --target x86_64-apple-ios-macabi $*
+# xargo build --target x86_64-apple-ios-macabi $*
+cargo +nightly run -Z build-std --target x86_64-apple-ios-macabi $*
 
